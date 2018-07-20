1d9f878b188f5dbc122b2d1b0a3551689242777a
Cristiano Silva <p534722@mail.caixa>
2018-07-20 10:18:03 -0300

    my commit

---
+++ CourseWebDeveloperCSS/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CourseWebDeveloperCSS.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aula03()
+        {
+            return View("Aula03");
+        }
+
+        public ActionResult Aula04()
+        {
+            return View("Aula04");
+        }
+    }
+}
